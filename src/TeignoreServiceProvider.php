c252b4c4d1b277683dadea8c7b4e54138a306621
司硕 <sis@golive.cc>
2020-05-19 10:21:49 +0800

    add tenant commit

---
+++ src/TeignoreServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Taokens\Teignore;
+
+use Illuminate\Support\ServiceProvider;
+
+class TeignoreServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $appPath = __DIR__.'/../TenantIgnore.php';
+
+        $this->publishes([
+            $appPath => app_path('Traits/TenantIgnore.php'),
+        ]);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ src/TenantIgnore.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Taokens\Teignore;
+
+
+trait TenantIgnore
+{
+    /**
+     * 模型事件处理
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($model){
+            $tenantId = request()->attributes->get('tenant_id');
+            $tenantCode = request()->attributes->get('tenant_code');
+            $model->tenant_id = $tenantId;
+            $model->tenant_code = $tenantCode;
+        });
+        static::addGlobalScope(function (object $model){
+            $tenantId = request()->attributes->get('tenant_id');
+            $tenantCode = request()->attributes->get('tenant_code');
+            $model->where('tenant_id', $tenantId);
+            $model->where('tenant_code', $tenantCode);
+        });
+        static::updating(function (object $model){
+            $tenantId = request()->attributes->get('tenant_id');
+            $tenantCode = request()->attributes->get('tenant_code');
+            $model->tenant_id = $tenantId;
+            $model->tenant_code = $tenantCode;
+        });
+    }
+}
