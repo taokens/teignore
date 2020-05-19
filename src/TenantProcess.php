e2f78b2e2ff3c76eba4bfb3872acc6b0fa5ba065
司硕 <sis@golive.cc>
2020-05-19 16:25:38 +0800

    Modify publishing path

--- src/TenantProcess.php
+++ src/TenantProcess.php
@@ -27,6 +27,13 @@ trait TenantProcess
             $model->tenant_code = $tenantCode;
         });
 		
+		static::deleting(function(object $model) {
+            $tenantId = request()->attributes->get('tenant_id');
+            $tenantCode = request()->attributes->get('tenant_code');
+            $model->where('tenant_id', $tenantId);
+            $model->where('tenant_code', $tenantCode);
+        });
+		
         static::addGlobalScope(function (object $model){
             $tenantId = request()->attributes->get('tenant_id');
             $tenantCode = request()->attributes->get('tenant_code');
