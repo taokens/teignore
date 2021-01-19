b9a9e0a5f590fd2c5905f585322e64cd405260c4
司硕 <sis@golive.cc>
2021-01-19 11:09:49 +0800

    增加获取钉钉用户信息接口

--- src/Api/UserInfo.php
+++ src/Api/UserInfo.php
@@ -18,4 +18,14 @@ class UserInfo extends TenantGateWay
     {
         return $this->setUrl(Golive::USER_GET_DETAIL)->send($params);
     }
+
+    /**
+     * SaaS获取钉钉用户信息
+     * @param array $params
+     * @return bool|string
+     */
+    public function getDingUserInfo(array $params = [])
+    {
+        return $this->setUrl(Golive::DING_USER_GET_DETAIL)->send($params);
+    }
 }
